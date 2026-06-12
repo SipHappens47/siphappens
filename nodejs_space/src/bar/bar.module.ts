c756b79c270cd76570b26c931eb1bb83eed766e4
AJ66 <aj.hartman47@gmail.com>
2026-06-12 16:25:23 +0200

    fix: import ConnectionsModule/CheersModule instead of re-providing services (deploy crash)

--- nodejs_space/src/bar/bar.module.ts
+++ nodejs_space/src/bar/bar.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { BarController } from './bar.controller';
 import { BarService } from './bar.service';
 import { PrismaService } from '../prisma/prisma.service';
-import { ConnectionsService } from '../connections/connections.service';
-import { CheersService } from '../cheers/cheers.service';
+import { ConnectionsModule } from '../connections/connections.module';
+import { CheersModule } from '../cheers/cheers.module';
 
 @Module({
+  imports: [ConnectionsModule, CheersModule],
   controllers: [BarController],
-  providers: [BarService, PrismaService, ConnectionsService, CheersService],
+  providers: [BarService, PrismaService],
 })
 export class BarModule {}
--- nodejs_space/src/cheers/cheers.module.ts
+++ nodejs_space/src/cheers/cheers.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { CheersController } from './cheers.controller';
 import { CheersService } from './cheers.service';
 import { PrismaService } from '../prisma/prisma.service';
-import { ConnectionsService } from '../connections/connections.service';
+import { ConnectionsModule } from '../connections/connections.module';
 
 @Module({
+  imports: [ConnectionsModule],
   controllers: [CheersController],
-  providers: [CheersService, PrismaService, ConnectionsService],
+  providers: [CheersService, PrismaService],
+  exports: [CheersService],
 })
 export class CheersModule {}
